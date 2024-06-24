70f6056f0c62585667b804563384901beaed71f8
Antonio del Toro <antonioedeltoro@gmail.com>
2024-06-23 21:10:33 -0400

    add comments to code functions

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,89 +1,129 @@
-const Campground = require("../models/campground");
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxtoken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessToken: mapBoxtoken });
-const { cloudinary } = require("../cloudinary");
+const Campground = require("../models/campground"); // Importing the Campground model
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding"); // Importing Mapbox geocoding service
+const mapBoxtoken = process.env.MAPBOX_TOKEN; // Accessing Mapbox token from environment variables
+const geocoder = mbxGeocoding({ accessToken: mapBoxtoken }); // Creating a new geocoding client with the token
+const { cloudinary } = require("../cloudinary"); // Importing Cloudinary for image upload management
 
-module.exports.index = async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
+// Index route: Fetch and display all campgrounds
+module.exports.index = async (req, res, next) => {
+  try {
+    const campgrounds = await Campground.find({}).populate("popupText"); // Fetch all campgrounds and populate popupText field
+    res.render("campgrounds/index", { campgrounds }); // Render the index template with the campgrounds
+  } catch (err) {
+    next(err); // Pass any errors to the next middleware
+  }
 };
 
+// Render new campground form
 module.exports.renderNewForm = (req, res) => {
-  res.render("campgrounds/new");
+  res.render("campgrounds/new"); // Render the new campground form template
 };
 
+// Create a new campground
 module.exports.createCampground = async (req, res, next) => {
-  const geoData = await geocoder
-    .forwardGeocode({
-      query: req.body.campground.location,
-      limit: 1,
-    })
-    .send();
-  const campground = new Campground(req.body.campground);
-  campground.geometry = geoData.body.features[0].geometry;
-  campground.images = req.files.map((f) => ({
-    url: f.path,
-    filename: f.filename,
-  }));
-  campground.author = req.user._id;
-  await campground.save();
-  console.log(campground);
-  req.flash("success", "Successfully made a new campground!");
-  res.redirect(`/campgrounds/${campground._id}`);
-};
+  try {
+    // Geocode the campground location
+    const geoData = await geocoder
+      .forwardGeocode({
+        query: req.body.campground.location, // Use location from the request body
+        limit: 1, // Limit the results to 1
+      })
+      .send();
 
-module.exports.showCampground = async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
-    .populate({
-      path: "reviews",
-      populate: {
-        path: "author",
-      },
-    })
-    .populate("author");
-  if (!campground) {
-    req.flash("error", "Cannot find that campground!");
-    return res.redirect("/campgrounds");
+    const campground = new Campground(req.body.campground); // Create a new Campground instance with the data from the request body
+    campground.geometry = geoData.body.features[0].geometry; // Set the geometry field with the geocoded data
+    campground.images = req.files.map((f) => ({
+      url: f.path,
+      filename: f.filename,
+    })); // Map the uploaded files to image objects
+    campground.author = req.user._id; // Set the author to the current user's ID
+    await campground.save(); // Save the new campground to the database
+
+    req.flash("success", "Successfully made a new campground!"); // Flash a success message
+    res.redirect(`/campgrounds/${campground._id}`); // Redirect to the new campground's show page
+  } catch (err) {
+    next(err); // Pass any errors to the next middleware
   }
-  res.render("campgrounds/show", { campground });
 };
 
-module.exports.renderEditForm = async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  if (!campground) {
-    req.flash("error", "Cannot find that campground!");
-    return res.redirect("/campgrounds");
-  }
-  res.render("campgrounds/edit", { campground });
-};
+// Show a single campground
+module.exports.showCampground = async (req, res, next) => {
+  try {
+    // Find the campground by ID and populate related fields
+    const campground = await Campground.findById(req.params.id)
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate("author");
 
-module.exports.updateCampground = async (req, res) => {
-  const { id } = req.params;
-  console.log(req.body);
-  const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
-  campground.images.push(...imgs);
-  await campground.save();
-  if (req.body.deleteImages) {
-    for (let filename of req.body.deleteImages) {
-      await cloudinary.uploader.destroy(filename);
+    if (!campground) {
+      req.flash("error", "Cannot find that campground!"); // Flash an error message if campground is not found
+      return res.redirect("/campgrounds"); // Redirect to the index page
     }
-    await campground.updateOne({
-      $pull: { images: { filename: { $in: req.body.deleteImages } } },
-    });
-    console.log(campground);
+
+    res.render("campgrounds/show", { campground }); // Render the show template with the campground
+  } catch (err) {
+    next(err); // Pass any errors to the next middleware
   }
-  req.flash("success", "Successfully updated campground!");
-  res.redirect(`/campgrounds/${campground._id}`);
 };
 
-module.exports.deleteCampground = async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  req.flash("success", "Successfully deleted campground");
-  res.redirect("/campgrounds");
+// Render edit campground form
+module.exports.renderEditForm = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const campground = await Campground.findById(id); // Find the campground by ID
+
+    if (!campground) {
+      req.flash("error", "Cannot find that campground!"); // Flash an error message if campground is not found
+      return res.redirect("/campgrounds"); // Redirect to the index page
+    }
+
+    res.render("campgrounds/edit", { campground }); // Render the edit template with the campground
+  } catch (err) {
+    next(err); // Pass any errors to the next middleware
+  }
+};
+
+// Update a campground
+module.exports.updateCampground = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    }); // Find the campground by ID and update with new data
+
+    const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename })); // Map the uploaded files to image objects
+    campground.images.push(...imgs); // Add new images to the campground's images array
+    await campground.save(); // Save the updated campground
+
+    if (req.body.deleteImages) {
+      for (let filename of req.body.deleteImages) {
+        await cloudinary.uploader.destroy(filename); // Delete images from Cloudinary
+      }
+      await campground.updateOne({
+        $pull: { images: { filename: { $in: req.body.deleteImages } } },
+      }); // Remove the deleted images from the campground's images array
+    }
+
+    req.flash("success", "Successfully updated campground!"); // Flash a success message
+    res.redirect(`/campgrounds/${campground._id}`); // Redirect to the updated campground's show page
+  } catch (err) {
+    next(err); // Pass any errors to the next middleware
+  }
+};
+
+// Delete a campground
+module.exports.deleteCampground = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id); // Find the campground by ID and delete it
+
+    req.flash("success", "Successfully deleted campground"); // Flash a success message
+    res.redirect("/campgrounds"); // Redirect to the index page
+  } catch (err) {
+    next(err); // Pass any errors to the next middleware
+  }
 };
