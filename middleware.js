a845d07563240bf6164fc3328297c00ea32ae1bb
Antonio del Toro <antonioedeltoro@gmail.com>
2024-06-24 00:00:35 -0400

    add comments

--- middleware.js
+++ middleware.js
@@ -1,60 +1,70 @@
-const { campgroundSchema, reviewSchema } = require('./schemas');
-const ExpressError = require('./utils/ExpressError');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+const { campgroundSchema, reviewSchema } = require("./schemas");
+const ExpressError = require("./utils/ExpressError");
+const Campground = require("./models/campground");
+const Review = require("./models/review");
 
+// Middleware to check if the user is authenticated
 module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl;
-        req.flash('error', 'You must be signed in first!');
-        return res.redirect('/login');
-    }
-    next();
-}
+  if (!req.isAuthenticated()) {
+    // Store the original URL in session to redirect after login
+    req.session.returnTo = req.originalUrl;
+    // Flash an error message
+    req.flash("error", "You must be signed in first!");
+    return res.redirect("/login");
+  }
+  next();
+};
 
+// Middleware to store the returnTo path from session in res.locals
 module.exports.storeReturnTo = (req, res, next) => {
-    if (req.session.returnTo) {
-        res.locals.returnTo = req.session.returnTo;
-    }
-    next();
-}
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
 
+// Middleware to validate campground data
 module.exports.validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    // Construct error messages from Joi validation
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400); // Throw an ExpressError for bad request
+  } else {
+    next(); // Proceed to the next middleware
+  }
+};
 
+// Middleware to check if the current user is the author of a campground
 module.exports.isAuthor = async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!')
-        return res.redirect(`/campgrounds/${id}`);
-    }
-    next();
-}
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
 
+// Middleware to check if the current user is the author of a review
 module.exports.isReviewAuthor = async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
-    }
-    next();
-}
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
 
+// Middleware to validate review data
 module.exports.validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    // Construct error messages from Joi validation
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400); // Throw an ExpressError for bad request
+  } else {
+    next(); // Proceed to the next middleware
+  }
+};
--- schemas.js
+++ schemas.js
@@ -1,6 +1,7 @@
 const BaseJoi = require("joi");
 const sanitizeHtml = require("sanitize-html");
 
+// Define an extension for Joi to sanitize HTML inputs
 const extension = (joi) => ({
   type: "string",
   base: joi.string(),
@@ -9,34 +10,38 @@ const extension = (joi) => ({
   },
   rules: {
     escapeHTML: {
-      validate(values, helpers) {
+      validate(value, helpers) {
         const clean = sanitizeHtml(value, {
-          allowedTags: [],
-          allowedAttributes: {},
+          allowedTags: [], // No tags allowed
+          allowedAttributes: {}, // No attributes allowed
         });
         if (clean !== value)
+          // If clean version is different, it means HTML was removed
           return helpers.error("string.escapeHTML", { value });
-        return clean;
+        return clean; // Return sanitized value
       },
     },
   },
 });
 
+// Extend BaseJoi with the defined extension
 const Joi = BaseJoi.extend(extension);
 
+// Export campgroundSchema for validating campground data
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required().escapeHTML(),
-    price: Joi.number().required().min(0),
-    location: Joi.string().required().escapeHTML(),
-    description: Joi.string().required().escapeHTML(),
+    title: Joi.string().required().escapeHTML(), // Validate title and sanitize HTML
+    price: Joi.number().required().min(0), // Validate price
+    location: Joi.string().required().escapeHTML(), // Validate location and sanitize HTML
+    description: Joi.string().required().escapeHTML(), // Validate description and sanitize HTML
   }).required(),
-  deleteImages: Joi.array(),
+  deleteImages: Joi.array(), // Optional array of images to delete
 });
 
+// Export reviewSchema for validating review data
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
-    rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required().escapeHTML(),
+    rating: Joi.number().required().min(1).max(5), // Validate rating
+    body: Joi.string().required().escapeHTML(), // Validate review body and sanitize HTML
   }).required(),
 });
