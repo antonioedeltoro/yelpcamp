e16a03521076c50e0471f524861c544828021b39
Antonio del Toro <antonioedeltoro@gmail.com>
2024-06-23 23:15:22 -0400

    add messages

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,9 +1,12 @@
+// Define a custom error class that extends the built-in Error class
 class ExpressError extends Error {
-    constructor(message, statusCode) {
-        super();
-        this.message = message;
-        this.statusCode = statusCode;
-    }
+  // Constructor to initialize the error with a message and status code
+  constructor(message, statusCode) {
+    super(); // Call the parent class's constructor
+    this.message = message; // Set the error message
+    this.statusCode = statusCode; // Set the status code
+  }
 }
 
+// Export the custom error class to be used in other parts of the application
 module.exports = ExpressError;
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,8 @@
-module.exports = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    }
-}
+// Export a higher-order function that wraps asynchronous route handlers
+module.exports = (func) => {
+  // Return a new function that takes req, res, and next as arguments
+  return (req, res, next) => {
+    // Call the provided function (func) with req, res, and next, and catch any errors
+    func(req, res, next).catch(next);
+  };
+};
