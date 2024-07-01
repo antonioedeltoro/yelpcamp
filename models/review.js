562cd94814d55ccb15143f516eccb7cb3fb671de
Antonio del Toro <antonioedeltoro@gmail.com>
2024-07-01 16:40:45 -0400

    add descriptions

--- models/review.js
+++ models/review.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
+// Import mongoose
+const mongoose = require("mongoose");
+
+// Create a Schema constructor
 const Schema = mongoose.Schema;
 
+// Define the Review schema
 const reviewSchema = new Schema({
-    body: String,
-    rating: Number,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
+  body: String, // The body of the review
+  rating: Number, // The rating given in the review
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User", // Reference to the User model
+  },
 });
 
+// Export the Review model based on the review schema
 module.exports = mongoose.model("Review", reviewSchema);
-
