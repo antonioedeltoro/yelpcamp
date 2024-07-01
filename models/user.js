d7cf3777549b2e1427a35e3fbd92ec7142f6173b
Antonio del Toro <antonioedeltoro@gmail.com>
2024-07-01 16:42:12 -0400

    add descriptions

--- models/user.js
+++ models/user.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+// Import mongoose
+const mongoose = require("mongoose");
 
+// Create a Schema constructor
+const Schema = mongoose.Schema;
+
+// Import passport-local-mongoose for handling username and password
+const passportLocalMongoose = require("passport-local-mongoose");
+
+// Define the User schema
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  email: {
+    type: String,
+    required: true, // Email is required
+    unique: true, // Email must be unique
+  },
 });
 
+// Add passport-local-mongoose plugin to the User schema to handle password hashing and authentication
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+// Export the User model based on the User schema
+module.exports = mongoose.model("User", UserSchema);
