4b4188e2904bb063b3c392c40596ab32bdada7e7
Antonio del Toro <antonioedeltoro@gmail.com>
2024-07-02 18:38:11 -0400

    add descriptions

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,9 @@
-module.exports = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    }
-}
+// Export a function that wraps an asynchronous function for error handling
+module.exports = (func) => {
+  // Return a new function that takes req, res, and next as parameters
+  return (req, res, next) => {
+    // Call the passed asynchronous function (func) with req, res, and next
+    // and catch any errors, passing them to the next middleware
+    func(req, res, next).catch(next);
+  };
+};
