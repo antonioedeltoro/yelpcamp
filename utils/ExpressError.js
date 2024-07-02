dfb48ebbfe11d5a0b2d0633a49eb44e07cf59b20
Antonio del Toro <antonioedeltoro@gmail.com>
2024-07-02 18:39:31 -0400

    add descriptions

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,9 +1,12 @@
+// Define a custom error class called ExpressError that extends the built-in Error class
 class ExpressError extends Error {
-    constructor(message, statusCode) {
-        super();
-        this.message = message;
-        this.statusCode = statusCode;
-    }
+  // Constructor takes a message and a statusCode as parameters
+  constructor(message, statusCode) {
+    super(); // Call the parent class constructor
+    this.message = message; // Set the error message
+    this.statusCode = statusCode; // Set the status code for the error
+  }
 }
 
+// Export the ExpressError class so it can be used in other modules
 module.exports = ExpressError;
